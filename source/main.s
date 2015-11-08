85a5bbbe1ff256f219329d964f540383ede61df3
Colleen Murphy <colleen@gazlene.net>
2015-11-07 23:45:45 -0800

    Lesson 5 - OK05
    SOS

--- source/main.s
+++ source/main.s
@@ -15,6 +15,14 @@ bl SetGpioFunction
 .unreq pinNum
 .unreq pinFunc
 
+@ Load the pattern sequence
+@ Initialize sequence index to 0
+ptrn .req r4
+ldr ptrn,=pattern
+ldr ptrn,[ptrn]
+seq .req r5
+mov seq,#0
+
 loop$:
 pinNum .req r0
 pinVal .req r1
@@ -24,18 +32,30 @@ bl SetGpio
 .unreq pinNum
 .unreq pinVal
 
-mov r0,#0x80000
+mov r0,#0x40000
 bl Wait
 
 pinNum .req r0
 pinVal .req r1
 mov pinNum,#16
+@ Set value to 1 if current is 1
 mov pinVal,#1
+lsl pinVal,seq
+and pinVal,ptrn
 bl SetGpio
 .unreq pinNum
 .unreq pinVal
 
-mov r0,#0x80000
+mov r0,#0x40000
 bl Wait
 
+add seq,#1
+cmp seq,#32
+moveq seq,#0
+
 b loop$
+
+.section .data
+.align 2
+pattern:
+.int 0b11111111101010100010001000101010
